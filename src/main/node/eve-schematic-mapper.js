5e99b4ac8c59345771aafdac65b8b23292d9d87a
Trenton D. Adams <trent@trentonadams.ca>
2016-12-25 00:19:59 -0700

    - tests not passing, committing just to save

--- src/main/node/eve-schematic-mapper.js
+++ src/main/node/eve-schematic-mapper.js
@@ -3,7 +3,7 @@
 // and writes out the file
 
 /**
- * The following SQL was use to export the data using MySQL Workbench.
+ * The following SQL was used to export the data using MySQL Workbench.
  *
  *
  SELECT ps.schematicID, schematicName, cycleTime, typeID, quantity, isInput
@@ -12,11 +12,8 @@
  WHERE ps.schematicID = pstm.schematicID
  ORDER BY schematicName;
  */
-var schematics = require('./eve-planet-schematics.json');
-var types = require('./eve-mapped-types.json');
 
-var fs = require("fs");
-var mappedTypes = {schematics: {}};
+//var fs = require("fs");
 
 /**
  * Basic form where the array entries from the file above go into either the
@@ -24,40 +21,88 @@ var mappedTypes = {schematics: {}};
  *
  * e.g. { "99": { "inputs": [], outputs: [] }
  */
-for (var index = 0; index < schematics.length; index++)
-{
-    var schematicID = schematics[index].schematicID;
-    if (mappedTypes.schematics[schematicID] == undefined)
-    {   // setup a copy of the schematic and setup inputs/outputs
-        mappedTypes.schematics[schematicID] = {};
-        mappedTypes.schematics[schematicID]['schematicName'] =
-            schematics[index].schematicName;
-        mappedTypes.schematics[schematicID]['schematicID'] =
-            schematics[index].schematicID;
-        mappedTypes.schematics[schematicID]['inputs'] = [];
-    }
 
-    if (schematics[index].isInput == 0)
-    {
-        // We need the typeID of the actual schematic, which is the output
-        // commodity, not any of the input commodity.  it's the output
-        // commodity that this schematic is for.
-        mappedTypes.schematics[schematicID]['typeID'] =
-            schematics[index].typeID;
-        mappedTypes.schematics[schematicID]['cycleTime'] =
-            schematics[index].cycleTime;
-        mappedTypes.schematics[schematicID]['outputQuantity'] =
-            schematics[index].quantity;
-    }
-    else
-    {
-        var schematic = Object.assign({}, schematics[index]);
-        schematic.schematicName = types[schematic.typeID].typeName;
-        mappedTypes.schematics[schematicID].inputs.push(schematic);
-    }
+function createNewSchematic(currentSchematic, mappedTypes)
+{
+    //console.log('generating new schematic: ', currentSchematic.schematicName);
+    var schematicID = Number(currentSchematic.schematicID);
+    /*
+     Sets up a schematic like...
+     {
+     schematicName: 'Bacteria',
+     schematicID: 131,
+     inputs: [ ],  // unhandled inputs so far
+     typeID: 2393,
+     cycleTime: 1800,
+     outputQuantity: 20
+     }
+     */
+    var mappedSchematic = {};
+    mappedSchematic['schematicName'] =
+        currentSchematic.schematicName;
+    mappedSchematic['schematicID'] = schematicID;
+    mappedSchematic['inputs'] = [];
+    // We need the typeID of the actual schematic, which is the output
+    // commodity, not any of the input commodity.  it's the output
+    // commodity that this schematic is for.
+    mappedSchematic['typeID'] =
+        Number(currentSchematic.typeID);
+    mappedSchematic['cycleTime'] =
+        Number(currentSchematic.cycleTime);
+    mappedSchematic['outputQuantity'] =
+        Number(currentSchematic.quantity);
+
+    mappedTypes.schematics[schematicID] = mappedSchematic;
+
+    return mappedSchematic;
 }
 
-mappedTypes['types'] = Object.assign({}, types);
 
-fs.writeFile('./eve-mapped-schematics.json', JSON.stringify(mappedTypes),
-    "utf-8");
+var schematicMapper = function (schematics, types)
+{
+    var schematicImports = require('./evedata/eve-planet-schematics.json');
+    var types = require('./evedata/eve-types.json');
+    var mappedTypes = {schematics: {}};
+
+    for (var index = 0; index < schematicImports.length; index++)
+    {
+        //console.log(schematics[index]);
+        var currentSchematic = schematicImports[index];
+        // the schematic type we're mapping to.
+        var schematicID = currentSchematic.schematicID;
+        // the mapped schematic.  Only created if isInput == 0, and reused
+        // if isInput == 1
+        var mappedSchematic;
+        /*console.log('schematic: ', currentSchematic.schematicName, ', isInput: ',
+         currentSchematic.isInput, ', schematicID: ', schematicID);
+         console.log('mappedSchematic: ', mappedSchematic);*/
+        if (currentSchematic.isInput == 0)
+        {   // setup a copy of the schematic and setup inputs/outputs
+            // the id of the schematic.
+            mappedSchematic = createNewSchematic(currentSchematic, mappedTypes);
+        }
+        else
+        {   // put the type in the inputs
+            /*            console.log('existing schematic:, ',
+             mappedTypes.schematics[schematicID]);*/
+            mappedSchematic = mappedTypes.schematics[schematicID];
+            var type = Object.assign({}, types[currentSchematic.typeID]);
+            //console.log('typeID:', currentSchematic.typeID);
+            if (schematicID == 131)
+            {
+                console.log('currentSchematic:', currentSchematic);
+                console.log('mappedSchematic:', mappedSchematic);
+            }
+            //console.log('type:', type);
+            //console.log(types[schematic.typeID]);
+            mappedSchematic.inputs.push(type);
+        }
+    }
+
+    mappedTypes['types'] = Object.assign({}, types);
+    return mappedTypes;
+};
+
+
+module.exports = schematicMapper;
+
--- src/main/node/eve-type-mapper.js
+++ src/main/node/eve-type-mapper.js
@@ -6,7 +6,7 @@
  * The following was used to export the eve types using MySQL workbench.
  SELECT * FROM evedata.invTypes
  */
-var types = require('./eve-types.json');
+var types = require('./evedata/eve-types.json');
 var fs = require("fs");
 var mappedTypes = {};
 
@@ -15,4 +15,4 @@ for (var index = 0; index < types.length; index++)
     mappedTypes[types[index].typeID] = types[index];
 }
 
-fs.writeFile('./eve-mapped-types.json', JSON.stringify(mappedTypes), "utf-8");
+fs.writeFile('./evedata/eve-mapped-types.json', JSON.stringify(mappedTypes), "utf-8");
