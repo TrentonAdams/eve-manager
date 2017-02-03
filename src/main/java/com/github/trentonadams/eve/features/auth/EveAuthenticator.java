ff062b15e72ec702f87783bf28dc98a843443403
Trenton D. Adams <trent@trentonadams.ca>
2017-02-02 23:52:58 -0700

    - remove todo comments, as we now have github issues

--- src/main/java/com/github/trentonadams/eve/features/auth/EveAuthenticator.java
+++ src/main/java/com/github/trentonadams/eve/features/auth/EveAuthenticator.java
@@ -30,12 +30,8 @@ import java.net.URISyntaxException;
 import java.util.Base64;
 
 /**
- * TODO look into possibly making a generic wrapper around eve calls
- * <p>
- * TODO provide client caching.  Use JCS and look at JAXRS book
- * <p>
- * TODO This class currently only supports authenticating a single character.
- * Refactor for support for multiple characters.
+ * Eve authenticator class.  Does the bulk of eve authentication after
+ * validating an eve code.
  */
 public final class EveAuthenticator
 {
